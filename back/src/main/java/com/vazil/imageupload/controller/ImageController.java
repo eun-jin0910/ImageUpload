fc9418a1a6b125654fbd5873423eec93d7843b20
EunJin Lee <dldmswls0910@naver.com>
2023-06-23 22:46:12 +0900

    [ETC] 파일 업로드 수정

--- back/src/main/java/com/vazil/imageupload/controller/ImageController.java
+++ back/src/main/java/com/vazil/imageupload/controller/ImageController.java
@@ -1,33 +1,17 @@
 package com.vazil.imageupload.controller;
 
-import com.vazil.imageupload.dto.ChunkRequestDTO;
 import com.vazil.imageupload.dto.ImageRequestDTO;
 import com.vazil.imageupload.dto.ImageResponseDTO;
 
 import com.vazil.imageupload.service.ImageService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.http.codec.multipart.Part;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.BodyExtractors;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.io.File;
-import java.nio.ByteBuffer;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @CrossOrigin("*")
 @RestController
@@ -49,34 +33,14 @@ public class ImageController {
         return imageService.getAllImages();
     }
 
-
-    // 일괄,순차,병렬 업로드
-
-//    @PostMapping(value = "/image", consumes = "multipart/form-data")
-//    public Flux<String> insertImage(@RequestPart("images") Flux<FilePart> filePartFlux,
-//                                    @RequestPart("password") String password,
-//                                    @RequestPart("title") String title) {
-//        log.info("***ImageController-insertImage***");
-//        return filePartFlux.flatMap(filePart -> {
-//            ImageRequestDTO imageDTO = new ImageRequestDTO(title, password, filePart);
-//            return imageService.createImage(Mono.just(imageDTO));
-//        });
-//    }
-
-
-    // 분할 업로드
-
     @PostMapping(value = "/image", consumes = "multipart/form-data")
-    public Mono<String> insertImage(@RequestPart("image") Mono<FilePart> filePartMono,
-                                    @RequestPart("fileName") String fileName,
-                                    @RequestPart("index") String index,
-                                    @RequestPart("end") String end,
+    public Flux<String> insertImage(@RequestPart("images") Flux<FilePart> filePartFlux,
                                     @RequestPart("password") String password,
                                     @RequestPart("title") String title) {
         log.info("***ImageController-insertImage***");
-        return filePartMono.flatMap(filePart -> {
-            ChunkRequestDTO chunkDTO = new ChunkRequestDTO(title, password, fileName, index, end, filePart);
-            return imageService.uploadImageChunk(Mono.just(chunkDTO));
+        return filePartFlux.flatMap(filePart -> {
+            ImageRequestDTO imageDTO = new ImageRequestDTO(title, password, filePart);
+            return imageService.createImage(Mono.just(imageDTO));
         });
     }
 
